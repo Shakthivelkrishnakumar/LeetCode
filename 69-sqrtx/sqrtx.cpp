c2c603fcccae174a6359284bd62810225bcdd8ac
Shakthivel K <94911521+Shakthivelkrishnakumar@users.noreply.github.com>
2024-02-01 10:42:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (8.98%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -2,9 +2,10 @@
 class Solution {
 public:
     long long int squareroot(int n){
-    int s=0;
-    if (n == 0 || n == 1)
+          if (n == 0 || n == 1)
             return n;
+    int s=0;
+  
     int e=n/2;
     long long int mid= s+ (e-s)/2;
     long long int ans =-1;
